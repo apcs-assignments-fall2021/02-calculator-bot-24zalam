326e0ecc1ec273ba50234f9c05681e4d8559a1a8
Zara Alam <24zalam@athenian.org>
2021-09-07 08:03:56 -0700

    yes

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,27 +4,53 @@ public class MyMain {
 
     // Calculates the median of the three inputs
     public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+        if (a > b && a< c || a>c && a < b ) {
+            return a;
+        } else if (b > a && b < c || b<a && b>c) {
+            return b;
+        } else {
+            return c;
+        }
+
+
     }
+
+
+
+
+
+
+
+
 
     // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+        if (Math.abs(a)>Math.abs(b)) {
+            return a;
+        }
+        else
+            return b;
     }
 
     // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
     // where "a" and "b" are the inputs to the method
-    public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+        public static double pythagoras(int a, int b) {
+            // REPLACE WITH YOUR CODE
+            double squared = a * a + b * b;
+            return (Math.sqrt(squared));
+
+        }
+
+
+            public static void main (String[]args) {
+                // You may want to keep these lines of code to test that your methods work
+                System.out.println(median(1, 2, 3)); // should be 2
+                System.out.println(magnitude(-7, -1)); // should be -7
+                System.out.println(pythagoras(3, 4)); // should be 5.0
+
+                Scanner scan = new Scanner(System.in);
+            }
     }
 
-    public static void main(String[] args) {
-        // You may want to keep these lines of code to test that your methods work
-        System.out.println(median(1, 2, 3)); // should be 2
-        System.out.println(magnitude(-7, -1)); // should be -7
-        System.out.println(pythagoras(3, 4)); // should be 5.0
 
-        Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
-    }
-}
+
